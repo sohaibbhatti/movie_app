761d888fb60afb379bb364a33bcd1cd45a237db1
Sohaib Bhatti <sohaibbbhatti@gmail.com>
2013-08-30 18:21:00 +0500

    base rails initialization

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MovieApp::Application.config.secret_key_base = '66dec1d0171d8a16d7393966dd9a24dce1827dc04e4f5ff56c1cf87f25782fe8a49b57024e4842c8ea729d5079a0d665b3eb99363f171c0a2f6a54aab14b28b4'
