ffba4acf41e1a1ab36cd1f41557f7e79648fe4ff
Sohaib Bhatti <sohaibbbhatti@gmail.com>
2013-09-01 02:58:50 +0500

    change reference scope in controllers

--- app/controllers/api/v1/movies_controller.rb
+++ app/controllers/api/v1/movies_controller.rb
@@ -2,27 +2,29 @@ class Api::V1::MoviesController < ApplicationController
 
   # POST /movies
   def create
-    content = JSON.load(request.body.read)
-    response = V1::MovieService.create content
+    content = parse_request_body
+    return json_format_error unless content
+    response = ::V1::MovieService.create content
     render json: response.result.to_json, status: response.status
   end
 
   # PUT /movies/:id
   def update
-    content = JSON.load(request.body.read)
-    response = V1::MovieService.update params[:id], content
+    content = parse_request_body
+    return json_format_error unless content
+    response = ::V1::MovieService.update params[:id], content
     render json: response.result.to_json, status: response.status
   end
 
   # DELETE /movies/:id
   def destroy
-    response = V1::MovieService.delete params[:id]
+    response = ::V1::MovieService.delete params[:id]
     render json: response.result.to_json, status: response.status
   end
 
   # GET /movies/:id
   def show
-    response = V1::MovieService.read params[:id]
+    response = ::V1::MovieService.read params[:id]
     render json: response.result.to_json, status: response.status
   end
 end
--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,7 +5,7 @@ class Api::V1::UsersController < ApplicationController
   def create
     content = parse_request_body
     return json_format_error unless content
-    response = V1::UserService.create content
+    response = ::V1::UserService.create content
     render json: response.result.to_json, status: response.status
   end
 
@@ -13,19 +13,19 @@ class Api::V1::UsersController < ApplicationController
   def update
     content = parse_request_body
     return json_format_error unless content
-    response = V1::UserService.update params[:id], content
+    response = ::V1::UserService.update params[:id], content
     render json: response.result.to_json, status: response.status
   end
 
   # DELETE /users/:id
   def destroy
-    response = V1::UserService.delete params[:id]
+    response = ::V1::UserService.delete params[:id]
     render json: response.result.to_json, status: response.status
   end
 
   # get /users/:id
   def show
-    response = V1::UserService.read params[:id]
+    response = ::V1::UserService.read params[:id]
     render json: response.result.to_json, status: response.status
   end
 end
